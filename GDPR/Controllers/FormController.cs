9d2360248c996fb8419f0188f42e5666d11428e6
Albion Venables <albion.venables@insplanet.com>
2018-05-14 16:21:58 +0200

    form

---
+++ GDPR/Controllers/FormController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MyLoanHomepage.Controllers
+{
+    public class FormsController : Controller
+    {
+
+		[HttpPost, ValidateAntiForgeryToken]
+		[Route("Thankyou")]
+		public IActionResult Thankyou(GDPR.Models.FormModel model)
+			{
+				ViewBag.Canonical = "/thankyou";
+				return View("thankyou", model);
+
+			}
+		//public class SamlHolderModel
+  //      {
+  //          [Required]
+  //          [Display(Name = "SAMLResponse")]
+  //          public string SAMLResponse { get; set; }
+  //      }
+		
+    }
+
+}
+
---
+++ GDPR/Models/FormModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GDPR.Models
+{
+	public class FormModel
+		{
+		public string CustomerCase { get; set; }
+		public string CustomerMessage { get; set; }
+		public string CustomerContact { get; set; }
+		
+
+		}
+	}
